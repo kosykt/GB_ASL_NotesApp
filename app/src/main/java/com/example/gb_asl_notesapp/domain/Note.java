94609a1988733777981bd526753b7c818978fe6c
kos_ykt <kosvasl09@gamil.com>
2021-06-12 15:33:38 +0900

    add details

--- app/src/main/java/com/example/gb_asl_notesapp/domain/Note.java
+++ app/src/main/java/com/example/gb_asl_notesapp/domain/Note.java
@@ -1,9 +1,12 @@
 package com.example.gb_asl_notesapp.domain;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.StringRes;
 
 //    базовый класс хранения записей
-public class Note {
+public class Note  implements Parcelable {
 
     @StringRes
     private final int name;
@@ -16,11 +19,39 @@ public class Note {
         this.memory = memory;
     }
 
+    protected Note(Parcel in) {
+        name = in.readInt();
+        memory = in.readInt();
+    }
+
+    public static final Creator<Note> CREATOR = new Creator<Note>() {
+        @Override
+        public Note createFromParcel(Parcel in) {
+            return new Note(in);
+        }
+
+        @Override
+        public Note[] newArray(int size) {
+            return new Note[size];
+        }
+    };
+
     public int getHeadline() {
         return name;
     }
 
-    public int getMemory() {
+    public int getText() {
         return memory;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(name);
+        dest.writeInt(memory);
     }
 }
---
+++ app/src/main/java/com/example/gb_asl_notesapp/ui/details/NoteDetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.gb_asl_notesapp.ui.details;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.gb_asl_notesapp.R;
+
+public class NoteDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_details);
+    }
+}
